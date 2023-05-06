9cb8aae0abd509f9c829ddfc9e6a73f54b09b0c6
Ivaschenko Dmitry <ivaschenkoda144@gmail.com>
2023-05-06 07:00:06 +0600

    delete build

--- net-utils/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/utils/net-utils
-# Build directory: /workspaces/utils/net-utils/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
